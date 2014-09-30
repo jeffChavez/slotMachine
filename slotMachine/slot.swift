bdbd26c4d2770ac97455b3ec4d104e5869b96dde
Jeff Chavez <Maluu@Jeffs-MacBook-Pro.local>
2014-09-29 21:38:32 -0700

    UI needs work!

---
+++ slotMachine/slot.swift
@@ -0,0 +1,16 @@
+//
+//  slot.swift
+//  slotMachine
+//
+//  Created by Jeff Chavez on 9/20/14.
+//  Copyright (c) 2014 Jeff Chavez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image: UIImage = UIImage(named: "Ace")
+    var isRed = true
+}
